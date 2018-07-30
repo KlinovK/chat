cc5705877815a58fe063b68a59360b9459ef7492
kost9klinov <kost9klinov>
2018-07-30 22:23:21 +0300

    added authService

---
+++ devslopes-chat/Services/AuthService.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  devslopes-chat
+//
+//  Created by Константин Клинов on 7/30/18.
+//  Copyright © 2018 Константин Клинов. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+}
